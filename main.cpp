536801f6dc36fdaa688e8396201df956d7885a55
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-05-29 08:56:24 +0200

    TODO list par personne dans main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,31 @@
 #include <iostream>
 #include "bst.h"
 
+
+//TODO Crausaz
+// - Valider les tests, en écrire de nouveaux si nécessaire
+// - Key const& min() const ;
+// - Key const& max() const ;
+// - void erase_min() ;
+// - void erase_max() ;
+// - void erase(Key const& k) noexcept ;
+
+//TODO Forestier
+// - Valider les tests, en écrire de nouveaux si nécessaire
+// - template<typename Fn> void visit_in_order(Fn f) const;
+// - void display_indented(std::ostream &s) const noexcept ;
+// -  void linearize() noexcept ;
+// -  void balance() noexcept ;
+
+//TODO Herzig
+// - bst(bst const &other);
+// - bst &operator=(bst const &other);
+// - ~bst();
+// - bool contains(Key const& k) const noexcept ; // recherche de l'élément
+
+//TODO Tout le monde
+// - Refactor le code
+
 using namespace std;
 
 int main() {
